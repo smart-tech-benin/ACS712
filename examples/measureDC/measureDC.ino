cc82e42b178d32faec297f8f886b039141fbca9a
AMOUSSOU Z. Kenneth <kenneth.amoussou@gmail.com>
2018-09-11 22:33:34 +0100

    Fix: example program to read DC current measurement

---
+++ examples/measureDC/measureDC.ino
@@ -0,0 +1,21 @@
+#include <ACS712.h>
+
+ACS712 mySensor = ACS712(A0);
+
+void setup(){
+    // Init serial communication
+    Serial.begin(9600);
+
+    // Initialize the sensor - calibration
+    mySensor.begin();
+}
+
+void loop(){
+    Serial.print("Raw: ");
+    Serial.print(mySensor.raw());
+    Serial.print(", Current: ");
+    Serial.print(mySensor.readDC(), 3);
+    Serial.print(" mA, Offset: ");
+    Serial.println(mySensor.getOffset(), 3);
+    delay(500);
+}
